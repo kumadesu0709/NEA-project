d1154a7477da55a4165ad2a347e5dfbf2a849f6d
kumadesu0709 <james.guan@abingdon.org.uk>
2024-08-09 17:37:40 +0800

    9/8/24

---
+++ link to excel.py
@@ -0,0 +1,10 @@
+from openpyxl.workbook import Workbook
+from openpyxl import load_workbook
+
+
+#load existing spreadsheet
+wb = load_workbook("/Users/jamesguan/Desktop/CS A level NEA/NEA-project/NEA test sheet.xlsx")
+#create the actual spreadsheet
+ws = wb.active
+
+print(ws["B2"].value)
